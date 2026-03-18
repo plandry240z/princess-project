28c8caa7a4f125682de7ac3130a693cb7ef0f519
plandry240z <159398849+plandry240z@users.noreply.github.com>
2026-03-17 23:13:24 -0700

    added styling

--- scripts/faq.js
+++ scripts/faq.js
@@ -2,7 +2,7 @@ fetch('./scripts/faq.json')
     .then(result => result.json())
     .then(faqs => {
         //pull in skeleton
-        const container = document.querySelector('.faq');
+        const container = document.querySelector('.faq-container');
 
         //map the questions and the answers
         container.innerHTML = faqs.map(faq => `
